e1f74f307dfc871144289a1b87f379492870215a
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-05-09 15:37:13 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,15 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <div class="hero min-h-screen" style="background-image: url(https://api.lorem.space/image/fashion?);">
+  <div class="hero min-h-screen"
+    style="background-image: url(https://firebasestorage.googleapis.com/v0/b/vue3-firebase9-blog-45c59.appspot.com/o/Home%2F2B955AD9-F4FB-44F4-8F29-B5E8C52FC5BE.jpeg?alt=media&token=5aaae039-99e3-487b-b86a-52667a4a3612);">
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
         <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
         <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
           exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn glass">Get Started</button>
+        <button class="btn glass">WELCOME</button>
       </div>
     </div>
   </div>
-  
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
