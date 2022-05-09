4c316e29ef5c06369fa55c94d2a536441587d2f2
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-05-09 15:42:56 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,8 @@
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <h1 class="mb-5 text-5xl font-bold">Hello There</h1>
+        <p class="mb-5">歡迎來到我的網頁</p>
         <button class="btn glass">WELCOME</button>
       </div>
     </div>
