80e9a91545efb0549b03ab6da123a7001deee505
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-05-12 10:38:09 +0800

    UPdate

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
-    </svg>
+  <div class="loading">
+    <span></span>
   </div>
 </template>
 
-<script setup>
-
+<script>
+export default {
+  name: "LoadingComponent",
+};
 </script>
 
 <style lang="scss" scoped>
