1580e91f7e72f173fc3f3fe158519d071a8a2f7a
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-06-02 12:31:08 +0800

    Npdate Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,7 +17,7 @@
           <li tabindex="0">
             <a
               class="btn btn-ghost justify-between text-gray-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white">
-              Services
+              portfolios
               <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
               </svg>
@@ -55,7 +55,7 @@
         </li>
         <li tabindex="0">
           <a class="btn btn-ghost text-gray-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white ">
-            Services
+            portfolios
             <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
               <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
             </svg>
