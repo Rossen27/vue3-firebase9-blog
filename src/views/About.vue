69712244a74e722bf9685db41b76c4baea0c297b
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-06-02 12:27:26 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,63 +13,37 @@
     </div>
   </header>
 
-  <div class="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
+  <div class="font-serif leading-normal mx-auto py-12 px-4 max-w-lg bg-gray-100/50">
     <p class="mb-6 text-xl md:text-2xl uppercase  dark:text-gray-300 dark:hover:text-white">About Rossen's Life .</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">Quae commodi reiciendis dignissimos. Rerum
-      molestiae hic dolores assumenda dolor! Corrupti
-      reiciendis maxime fuga, recusandae obcaecati officia cum temporibus dicta quibusdam praesentium, magni, cumque
-      aperiam iusto sequi illum molestiae non.</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">In excepturi repellendus eum, qui corrupti rerum
-      perferendis harum adipisci voluptate?
-      Nihil, quidem deleniti libero officia dicta vel asperiores velit molestiae blanditiis, dolore voluptatibus
-      excepturi laudantium at veniam illo. Dolor!</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">Neque laudantium minus doloremque id quas quod
-      sint velit corporis unde dolore accusantium
-      delectus optio consequuntur voluptatem sapiente odit dolorum minima harum tenetur, dolor provident reprehenderit.
-      Ex eum provident harum?</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">Mollitia temporibus maxime placeat culpa
-      distinctio possimus, praesentium, assumenda quasi
-      eum voluptate magni aspernatur aperiam. Eius voluptates rem eum, facilis inventore hic provident pariatur nam non!
-      At odit iste cum.</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">Cumque voluptatibus laboriosam tempore architecto
-      laudantium sint vitae cupiditate voluptate
-      quod tempora saepe odio quasi dolores possimus non totam unde voluptates corrupti, ducimus ipsa enim officiis
-      ipsum maxime eveniet. Aut!</p>
-    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">Maxime facere ut natus libero incidunt alias quam
-      consectetur, nisi delectus exercitationem
-      ab qui perferendis dolorem sequi veritatis nobis eius quas dolore ducimus atque vel. Doloribus mollitia non
-      pariatur modi?</p>
-    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">Sed in amet a neque, delectus, cupiditate ab
-      assumenda quidem facere molestias vitae itaque
-      soluta reprehenderit dolorem. Deserunt quibusdam, consequatur veniam fugiat ipsam aliquam ea possimus quis
-      officiis enim dolores.</p>
+    <p class="mb-4 text-lg  dark:text-gray-300 dark:hover:text-white">
+      出生於宜蘭，為人相當熱情活潑，也樂於助人，做事俐落遇到問題也會想先找方法解決，遇到需要幫助的人都毫不吝嗇，喜歡分享自己的資源給需要的夥伴，在團體裡都是擔任開心果以及智多星的角色，自己的信條就是：
+      <br>&rdquo;Trust
+      yourself You know more than you think you do.&rdquo;<br>
+      所以相信凡事都是勤能補拙。</p>
   </div>
 
   <div class="bg-quote container flex items-center justify-center h-screen mx-auto">
     <blockquote class="font-serif mx-4 p-4 text-center text-white md:p-8 bg-gray-400/50 rounded-full">
       <p class="font-bold italic text-3xl">
-        &ldquo;Large, bold pull quote. Hats. Very good.&rdquo;
+        &ldquo;Trust
+      yourself You know more than you think you do.&rdquo;
       </p>
     </blockquote>
   </div>
 
-  <div class="font-serif leading-normal mx-auto py-12 px-4 max-w-lg">
-    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">Quae commodi reiciendis dignissimos. Rerum
-      molestiae hic dolores assumenda dolor! Corrupti
-      reiciendis maxime fuga, recusandae obcaecati officia cum temporibus dicta quibusdam praesentium, magni, cumque
-      aperiam iusto sequi illum molestiae non.</p>
-    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">In excepturi repellendus eum, qui corrupti rerum
-      perferendis harum adipisci voluptate?
-      Nihil, quidem deleniti libero officia dicta vel asperiores velit molestiae blanditiis, dolore voluptatibus
-      excepturi laudantium at veniam illo. Dolor!</p>
-    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">Neque laudantium minus doloremque id quas quod sint
-      velit corporis unde dolore accusantium
-      delectus optio consequuntur voluptatem sapiente odit dolorum minima harum tenetur, dolor provident reprehenderit.
-      Ex eum provident harum?</p>
-    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">Mollitia temporibus maxime placeat culpa distinctio
-      possimus, praesentium, assumenda quasi
-      eum voluptate magni aspernatur aperiam. Eius voluptates rem eum, facilis inventore hic provident pariatur nam non!
-      At odit iste cum.</p>
+  <div class="font-serif leading-normal mx-auto py-12 px-4 max-w-lg bg-gray-100/50">
+    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">
+      我出生於1990年，宜蘭人。在家中排行老大，下有妹妹和弟弟，因父母在傳統市場工作時常早出晚歸，所以我算是給阿嬤帶大；但我爸媽從小教育我，作為「生意人的孩子」，凡事靠自己，就此培養出我獨立與堅毅的性格。</p>
+    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">
+      在大學期間我就讀時尚與創意產業品牌建構及經營管理系（夜間部），由於不想給家人太多負擔，求學期間我開啟了半工半讀的生活直到畢業。</p>
+    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">
+      畢業後我的第一份正職工作－艾玖國際有限公司行銷助理（正職）職務，在公司時常使用PhotoShop及Illustrator等影像處理軟體，主要負責設計公司形象平面媒體（如：海報、文宣、廣告看板及活動打卡小物等）設計、美術編輯、庶務、庫存管理、粉絲團維繫及管理、百貨櫃人員管理、活動規劃、商品拍攝、會計等工作。
+    </p>
+    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">因為公司規模並不大的關係，我在此公司要學的以及處理的事務相當的多，也練就我一身十八般武藝樣樣略懂。
+    </p>
+    <p class="mb-4 text-lg dark:text-gray-300 dark:hover:text-white">
+      中間原本兼職保險業務人員後來因為業務需求增加便改專職於新光人壽了，在新光第一年就取得107年度13工作月業績競賽表現優異，個人業績新人組：第一名，爾後因家人身體無法再太勞累的關係變回家幫忙，中間也利用時間去學習咖啡相關技能，在短期內取的得國際烘豆師以及咖啡飲品調配師的相關證證，因個性好學的關係又接著學習網頁設計工程相關課程便開始對前端工程相當感興趣，遇到問題時也能以最快的速度找到解決方法。
+    </p>
   </div>
 
 </template>
