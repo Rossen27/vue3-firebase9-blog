dee0821d5518dde426c43c411313b887647417a8
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-06-06 15:11:15 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -19,8 +19,8 @@ const router = createRouter({
       component: () => import('@/views/MaskmapView.vue'),
     },
     {
-      path: '/weater',
-      component: () => import('@/views/WeaterView.vue'),
+      path: '/weather',
+      component: () => import('@/views/WeatherView.vue'),
     },
     {
       path: '/login',
