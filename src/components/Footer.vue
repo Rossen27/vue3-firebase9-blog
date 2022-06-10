dab012529faf9714862115c4700169c86ade481e
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-06-11 07:30:25 +0800

    UPdate

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 
   <footer class="bg-white dark:bg-gray-800 w-full py-8">
     <div class="max-w-screen-xl mx-auto px-4">
-      <ul class="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
+      <!-- <ul class="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
         <li class="my-2">
           <router-link to="/about"
             class="text-gray-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200"
@@ -28,7 +28,7 @@
             LinkedIn
           </a>
         </li>
-      </ul>
+      </ul> -->
       <div class="pt-8 flex max-w-xs mx-auto items-center justify-between">
         <a href="#" class="text-gray-400 hover:text-gray-500 dark:hover:text-white transition-colors duration-200">
           <svg width="20" height="20" fill="currentColor"
